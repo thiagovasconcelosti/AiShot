747da8e901cdd87d18518356733ade95ad0c6f41
Thiago Vasconcelos <bardjow@gmail.com>
2026-07-04 11:33:31 -0300

    build: checksum do instalador 0.1.2 (com hardening de update)

--- chocolatey/tools/chocolateyinstall.ps1
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $packageArgs = @{
   packageName    = 'aishot'
   fileType       = 'exe'
   url            = 'https://github.com/thiagovasconcelosti/AiShot/releases/download/v0.1.2/AiShot-Setup-0.1.2.exe'
-  checksum       = 'A88E8A18A8FE4A3AA4BECE30E5DC5FD4C1F4911405F45DA6252BC86B025C959D'
+  checksum       = 'B68053B20E61EBFBA56DFA1D179A3A29D91F5D9128778B27456A3070BF5E5D53'
   checksumType   = 'sha256'
   # Inno Setup: instalação silenciosa
   silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
